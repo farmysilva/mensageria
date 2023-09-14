48d3e6a5c4f7cf19f4b32b127c9dac191ccbb849
Farmy Silva <farmysilva@yahoo.com.br>
2023-09-13 21:50:40 -0300

    :construction_worker: BUILD: API's unificadas

---
+++ src/main/java/br/mil/fab/compraer/api/mensageria/MensageriaApplication.java
@@ -0,0 +1,13 @@
+package br.mil.fab.compraer.api.mensageria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensageriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensageriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/mil/fab/compraer/api/mensageria/MensageriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mil.fab.compraer.api.mensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
